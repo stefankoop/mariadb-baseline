2ba8644d0f4089b2c9f909236a7ab1a8e3e55c95
Stefan Koop <stefan.koop@wiredminds.de>
2020-11-03 23:20:36 +0100

    migrating to mariadb

--- controls/mysql_conf.rb
+++ controls/mysql_conf.rb
@@ -1,8 +1,9 @@
 # encoding: utf-8
 # frozen_string_literal: true
-
+#
 # Copyright 2014, Deutsche Telekom AG
 # Copyright 2018, Christoph Hartmann
+# Copyright 2020, Stefan Koop
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,16 +16,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-mysql_hardening_file = '/etc/mysql/conf.d/hardening.cnf'
-
-user = input('user')
-pass = input('password')
-
-# get mysql version and distribution
-version = mysql_version(user, pass)
-distribution = mysql_distribution(user, pass)
+# mysql_hardening_file = '/etc/mysql/conf.d/hardening.cnf'
 
 # get datadir and logfile-path from settings in the configuration if it is defined or from mysql itself
 mysql_data_path = if mysql_conf&.params&.mysqld&.datadir
@@ -36,7 +29,7 @@ mysql_data_path = if mysql_conf&.params&.mysqld&.datadir
 mysql_log_file = if mysql_conf.params.mysqld && mysql_conf.params.mysqld['log-error']
                    mysql_conf.params.mysqld['log-error']
                  else
-                   command("mysql -u#{user} -p#{pass} -sN -e \"select @@GLOBAL.log_error\";").stdout.strip
+                   command("mysql -sN -e \"select @@GLOBAL.log_error\";").stdout.strip
                  end
 
 # set OS-dependent filenames and paths
@@ -54,7 +47,7 @@ when 'redhat', 'fedora'
   service_name = 'mariadb' if os[:release] >= '7'
 end
 
-control 'mysql-conf-01' do
+control 'mariadb-conf-01' do
   impact 0.5
   title 'ensure the service is enabled and running'
   describe service(service_name) do
@@ -64,18 +57,18 @@ control 'mysql-conf-01' do
 end
 
 # 'Check for multiple instances' do
-control 'mysql-conf-02' do
+control 'mariadb-conf-02' do
   impact 0.5
-  title 'only one instance of mysql should run on a server'
+  title 'only one instance of mariadb should run on a server'
   describe command('ps aux | grep mysqld | egrep -v "grep|mysqld_safe|logger" | wc -l') do
     its(:stdout) { should match(/^1$/) }
   end
 end
 
 # Parsing configfiles for unwanted entries
-control 'mysql-conf-03' do
+control 'mariadb-conf-03' do
   impact 0.7
-  title 'enable secure configurations for mysql'
+  title 'enable secure configurations for mariadb'
   describe mysql_conf.params('mysqld') do
     its('safe-user-create') { should cmp 1 }
     its('old_passwords') { should_not cmp 1 }
@@ -89,20 +82,8 @@ control 'mysql-conf-03' do
   end
 end
 
-# Parsing configfiles for unwanted entries
-control 'mysql-conf-03b' do
-  impact 0.7
-  title 'enable secure configurations for mysql'
-  only_if('MySQL version less than 8.0.3 and not mariadb') do
-    (Gem::Version.new(version.mysql_version) <= Gem::Version.new('8.0.3') and distribution.mysql_distribution == 'mysql')
-  end
-  describe mysql_conf.params('mysqld') do
-    its('secure-auth') { should cmp 1 }
-  end
-end
-
 # 'Mysql-config: owner, group and permissions'
-control 'mysql-conf-04' do
+control 'mariadb-conf-04' do
   impact 0.7
   title 'ensure the mysql data path is owned by mysql user'
   describe file(mysql_data_path) do
@@ -112,7 +93,7 @@ control 'mysql-conf-04' do
   end
 end
 
-control 'mysql-conf-05' do
+control 'mariadb-conf-05' do
   impact 0.5
   title 'ensure data path is owned by mysql user'
   describe file("#{mysql_data_path}/ibdata1") do
@@ -124,7 +105,7 @@ control 'mysql-conf-05' do
   end
 end
 
-control 'mysql-conf-06' do
+control 'mariadb-conf-06' do
   impact 0.5
   title 'ensure log file is owned by mysql user'
   describe file(mysql_log_file) do
@@ -136,7 +117,7 @@ control 'mysql-conf-06' do
   end
 end
 
-control 'mysql-conf-07' do
+control 'mariadb-conf-07' do
   impact 0.7
   title 'ensure the mysql config file is owned by user root, group mysql'
   describe file(mysql_config_file) do
@@ -147,20 +128,20 @@ control 'mysql-conf-07' do
   end
 end
 
-# test this only if we have a mysql_hardening_file
-control 'mysql-conf-08' do
-  impact 0.5
-  title 'ensure the mysql hardening config file is owned by user root, group mysql'
-  describe file(mysql_hardening_file) do
-    it { should be_owned_by 'root' }
-    it { should be_grouped_into 'mysql' }
-    it { should_not be_readable.by('others') }
-  end
-  only_if { command("ls #{mysql_hardening_file}").exit_status.zero? }
-end
+## test this only if we have a mysql_hardening_file
+#control 'mysql-conf-08' do
+#  impact 0.5
+#  title 'ensure the mysql hardening config file is owned by user root, group mysql'
+#  describe file(mysql_hardening_file) do
+#    it { should be_owned_by 'root' }
+#    it { should be_grouped_into 'mysql' }
+#    it { should_not be_readable.by('others') }
+#  end
+#  only_if { command("ls #{mysql_hardening_file}").exit_status.zero? }
+#end
 
 # 'Mysql environment'
-control 'mysql-conf-09' do
+control 'mariadb-conf-09' do
   impact 1.0
   title 'the use of MYSQL_PWD is insecure and can exploit the password'
   describe command('env') do
--- controls/mysql_db.rb
+++ controls/mysql_db.rb
@@ -3,6 +3,7 @@
 
 # Copyright 2014, Deutsche Telekom AG
 # Copyright 2018, Christoph Hartmann
+# Copyright 2020, Stefan Koop
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,85 +16,81 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-user = input('user')
-pass = input('password')
+#control 'mysql-db-01' do
+#  impact 0.3
+#  title 'use supported mysql version in production'
+#  describe command("mysql -u#{user} -p#{pass} -sN -e 'select version();'") do
+#    its(:stdout) { should_not match(/Community/) }
+#  end
+#end
 
-control 'mysql-db-01' do
-  impact 0.3
-  title 'use supported mysql version in production'
-  describe command("mysql -u#{user} -p#{pass} -sN -e 'select version();'") do
-    its(:stdout) { should_not match(/Community/) }
-  end
-end
-
-control 'mysql-db-02' do
+control 'mariadb-db-02' do
   impact 0.5
-  title 'use mysql version 5 or higher'
-  describe command("mysql -u#{user} -p#{pass} -sN -e 'select substring_index(version(),\".\",1);'") do
-    its(:stdout) { should cmp >= 5 }
+  title 'use mariadb version 10.2 or higher'
+  describe command("mysql -sN -e 'select substring_index(version(),\".\",2);'") do
+    its(:stdout) { should cmp >= 10.2 }
   end
 end
 
-control 'mysql-db-03' do
+control 'mariadb-db-03' do
   impact 1.0
   title 'test database must be deleted'
-  describe command("mysql -u#{user} -p#{pass} -sN -e 'show databases like \"test\";'") do
+  describe command("mysql -sN -e 'show databases like \"test\";'") do
     its(:stdout) { should_not match(/test/) }
   end
 end
 
-control 'mysql-db-04' do
+control 'mariadb-db-04' do
   impact 1.0
   title 'deactivate annonymous user names'
-  describe command("mysql -u#{user} -p#{pass} -sN -e 'select count(*) from mysql.user where user=\"\";'") do
+  describe command("mysql -sN -e 'select count(*) from mysql.user where user=\"\";'") do
     its(:stdout) { should match(/^0/) }
   end
 end
 
-# MySQL 5.7.6 dropped the "password" column in the mysql.user table
-# so we have to check if it's there before we check if a password is empty
-control 'mysql-db-05' do
+## MySQL 5.7.6 dropped the "password" column in the mysql.user table
+## so we have to check if it's there before we check if a password is empty
+control 'mariadb-db-05' do
   impact 1.0
   title 'default passwords must be changed'
-  only_if { command("mysql -u#{user} -p#{pass} -sN -e 'select count(*) from information_schema.columns where table_name=\"user\" and table_schema=\"mysql\" and column_name=\"password\";'").stdout.strip == '1' }
-  describe command("mysql -u#{user} -p#{pass} -sN -e 'select count(*) from mysql.user where (length(password)=0 or password=\"\") and (length(authentication_string)=0 or authentication_string=\"\");'") do
+  only_if { command("mysql -sN -e 'select count(*) from information_schema.columns where table_name=\"user\" and table_schema=\"mysql\" and column_name=\"password\";'").stdout.strip == '1' }
+  describe command("mysql -sN -e 'select count(*) from mysql.user where (length(password)=0 or password=\"\") and (length(authentication_string)=0 or authentication_string=\"\");'") do
     its(:stdout) { should match(/^0/) }
   end
 end
 
 # MySQL versions older than 5.7.6 and MariaDB databases still have the
 # password column so we need to check if it is empty
-control 'mysql-db-05b' do
+control 'mariadb-db-05b' do
   impact 1.0
   title 'default passwords must be changed'
-  only_if { command("mysql -u#{user} -p#{pass} -sN -e 'select count(*) from information_schema.columns where table_name=\"user\" and table_schema=\"mysql\" and column_name=\"password\";'").stdout.strip == '0' }
-  describe command("mysql -u#{user} -p#{pass} -sN -e 'select count(*) from mysql.user where length(authentication_string)=0 or authentication_string=\"\";'") do
+  only_if { command("mysql -sN -e 'select count(*) from information_schema.columns where table_name=\"user\" and table_schema=\"mysql\" and column_name=\"password\";'").stdout.strip == '0' }
+  describe command("mysql -sN -e 'select count(*) from mysql.user where length(authentication_string)=0 or authentication_string=\"\";'") do
     its(:stdout) { should match(/^0/) }
   end
 end
 
-control 'mysql-db-06' do
+control 'mariadb-db-06' do
   impact 0.5
   title 'the grant option must not be used'
-  describe command("mysql -u#{user} -p#{pass} -sN -e 'select count(*) from mysql.user where grant_priv=\"y\" and User!=\"root\" and User!=\"debian-sys-maint\";'") do
+  describe command("mysql -sN -e 'select count(*) from mysql.user where grant_priv=\"y\" and User!=\"root\" and User!=\"debian-sys-maint\";'") do
     its(:stdout) { should match(/^0/) }
   end
 end
 
-control 'mysql-db-07' do
+control 'mariadb-db-07' do
   impact 0.5
   title 'ensure no wildcards are used for hostnames'
-  describe command("mysql -u#{user} -p#{pass} -sN -e 'select count(*) from mysql.user where host=\"%\"'") do
+  describe command("mysql -sN -e 'select count(*) from mysql.user where host=\"%\"'") do
     its(:stdout) { should match(/^0/) }
   end
 end
 
-control 'mysql-db-08' do
+control 'mariadb-db-08' do
   impact 0.5
   title 'it must be ensured that superuser can login via localhost only'
-  describe command("mysql -u#{user} -p#{pass} -sN -e 'select count(*) from mysql.user where user=\"root\" and host not in (\"localhost\",\"127.0.0.1\",\"::1\")'") do
+  describe command("mysql -sN -e 'select count(*) from mysql.user where user=\"root\" and host not in (\"localhost\",\"127.0.0.1\",\"::1\")'") do
     its(:stdout) { should match(/^0/) }
   end
 end
